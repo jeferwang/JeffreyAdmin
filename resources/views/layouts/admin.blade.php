8bbfd95865f0d6f2a73412a09680ff5e71180c75
Jessee <1737723363@qq.com>
2017-07-18 17:00:33 +0800

    修改后台template和css文件

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="zh_CN">
 
 <head>
 	<title>@yield('title')</title>
@@ -87,23 +87,14 @@
 			<nav>
 				<ul class="nav">
 					<li><a href="index.html" class="active"><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
-					<li><a href="elements.html" class=""><i class="lnr lnr-code"></i> <span>Elements</span></a></li>
-					<li><a href="charts.html" class=""><i class="lnr lnr-chart-bars"></i> <span>Charts</span></a></li>
-					<li><a href="panels.html" class=""><i class="lnr lnr-cog"></i> <span>Panels</span></a></li>
-					<li><a href="notifications.html" class=""><i class="lnr lnr-alarm"></i> <span>Notifications</span></a></li>
 					<li>
 						<a href="#subPages" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Pages</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
 						<div id="subPages" class="collapse ">
 							<ul class="nav">
 								<li><a href="page-profile.html" class="">Profile</a></li>
-								<li><a href="page-login.html" class="">Login</a></li>
-								<li><a href="page-lockscreen.html" class="">Lockscreen</a></li>
 							</ul>
 						</div>
 					</li>
-					<li><a href="tables.html" class=""><i class="lnr lnr-dice"></i> <span>Tables</span></a></li>
-					<li><a href="typography.html" class=""><i class="lnr lnr-text-format"></i> <span>Typography</span></a></li>
-					<li><a href="icons.html" class=""><i class="lnr lnr-linearicons"></i> <span>Icons</span></a></li>
 				</ul>
 			</nav>
 		</div>
