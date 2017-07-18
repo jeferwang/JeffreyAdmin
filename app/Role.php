881cf5328d644dab587be35d70c778083c34125f
Jessee <1737723363@qq.com>
2017-07-18 17:09:46 +0800

    编写添加Role的方法

--- app/Role.php
+++ app/Role.php
@@ -8,5 +8,13 @@ use Zizaco\Entrust\Traits\EntrustRoleTrait;
 class Role extends Model
 {
 	use EntrustRoleTrait;
-    //
+	
+	public static function addRole($name,$display_name,$description=null)
+	{
+		$role=new Role();
+		$role->name=$name;
+		$role->display_name=$display_name;
+		$role->description=$description;
+		$role->save();
+	}
 }
