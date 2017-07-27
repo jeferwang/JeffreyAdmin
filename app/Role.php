682fb3f58b3fbdb41490cc5faecd113b718562d3
Jessee <1737723363@qq.com>
2017-07-28 00:29:36 +0800

    去除多余的关联关系

--- app/Role.php
+++ app/Role.php
@@ -12,27 +12,16 @@ class Role extends Model
 	protected $fillable = ['name', 'display_name', 'description'];
 	
 	/**
-	 * Role和User的关联关系
-	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+	 * 删除角色
+	 * 1,解除和所有相关权限的关系
+	 * 2,解除和所有相关用户的关系
+	 * 3,删除角色本身
+	 * @return mixed
 	 */
-	public function users()
-	{
-		return $this->belongsToMany('App\User');
-	}
-	
-	/**
-	 * Role和Permission的关联关系
-	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-	 */
-	public function permissions()
-	{
-		return $this->belongsToMany('App\Permission');
-	}
-	
 	public function delRole()
 	{
 		// 解除角色和权限的关联关系
-		$this->detachPermissions($this->permissions);
+		$this->detachPermissions($this->perms);
 		// 解除每个相关用户与本角色的关联关系
 		foreach ($this->users as $user) {
 			$user->detachRole($this);
