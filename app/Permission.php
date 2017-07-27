64e09c385bf6f99885335dbbb6b20c0f1738fa06
Jessee <1737723363@qq.com>
2017-07-28 00:29:55 +0800

    添加删除权限的方法

--- app/Permission.php
+++ app/Permission.php
@@ -8,5 +8,20 @@ use Zizaco\Entrust\Traits\EntrustPermissionTrait;
 class Permission extends Model
 {
 	use EntrustPermissionTrait;
-    //
+	
+	/**
+	 * 删除权限
+	 * 1,解除和角色的关系
+	 * 2,删除权限本身
+	 * @return bool|null
+	 */
+	public function delPermission()
+	{
+		// 解除和角色的关系
+		foreach ($this->roles as $role) {
+			$role->detachPermission($this);
+		}
+		// 删除权限本身
+		return $this->delete();
+	}
 }
