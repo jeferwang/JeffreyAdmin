a5a1753f6c71b51ed39d2cdaed6dafb057ae39fd
Jessee <1737723363@qq.com>
2017-07-30 14:42:41 +0800

    添加前后台切换的链接

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth'], function () {
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['auth', 'role:后台管理员']], function () {
 	// IndexController
 	Route::group(['prefix' => 'index', 'as' => 'index.'], function () {
 		Route::get('index', 'Admin\IndexController@index')->name('index');
