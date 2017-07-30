3514700beceea05285e8f704dcaf2d3011a1fd53
Jessee <1737723363@qq.com>
2017-07-30 14:43:11 +0800

    删除无用注释

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -81,9 +81,6 @@
                             <li><a href="#"><i class="lnr lnr-exit"></i> <span>Logout</span></a></li>
                         </ul>
                     </li>
-                    <!-- <li>
-                        <a class="update-pro" href="#downloads/klorofil-pro-bootstrap-admin-dashboard-template/?utm_source=klorofil&utm_medium=template&utm_campaign=KlorofilPro" title="Upgrade to Pro" target="_blank"><i class="fa fa-rocket"></i> <span>UPGRADE TO PRO</span></a>
-                    </li> -->
                 </ul>
             </div>
         </div>
